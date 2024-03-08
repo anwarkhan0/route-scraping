348d09dcebd819cd62350fd28324881abffacfbf
Syed Anwar <anwarsyed01234@gmail.com>
2024-03-08 12:21:43 +0500

    mod: text Chunking removed

--- index.js
+++ index.js
@@ -11,15 +11,12 @@ import { aiExtract } from "./aiExtracter.js";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { scrape } from "./scrape.js";
 
-
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 
 const __dirname = process.cwd();
-
-
 
 app.get("/", (req, res) => {
   try {
@@ -29,7 +26,6 @@ app.get("/", (req, res) => {
     res.status(401).send("page not found");
   }
 });
-
 
 app.get("/display-routes", async (req, res) => {
   res.sendFile(path.join(__dirname, "./routesList.html"));
@@ -47,7 +43,7 @@ app.get("/scrape", async (req, res) => {
     let data = await scrape(url);
 
     if (!data) {
-      return res.status(404).json({ message: 'No data found' });
+      return res.status(404).json({ message: "No data found" });
     }
 
     console.log(
@@ -56,57 +52,36 @@ app.get("/scrape", async (req, res) => {
 
     let results = [];
     // for (let i = 0; i < contents.length; i++) {
-      console.log("Extraction of page ");
+    console.log("Extraction of page ");
 
-      if (data === undefined) {
-        console.log(`data is undefined`);
-        // continue;
-      }
+    if (data === undefined) {
+      console.log(`data is undefined`);
+      // continue;
+    }
 
-      data+= 'Website = ' + url;
+    data += "Website = " + url;
 
-      const tokens = encode(data);
-      const tokenCount = tokens.length;
+    const tokens = encode(data);
 
-      if (tokenCount > 3000) {
-        const splitter = new RecursiveCharacterTextSplitter({
-          chunkSize: 2500,
-          chunkOverlap: 1,
-        });
-
-        const docs = await splitter.createDocuments([data]);
-
-        for (let i = 0; i < docs.length; i++) {
-          const result = await aiExtract(docs[i]);
-          results = results.concat(result.routes || []);
-        }
-      } else {
-        const result = await aiExtract(data);
-        results = results.concat(result.routes || []);
-      }
-    // }
+    const result = await aiExtract(data);
+    results = results.concat(result.routes || []);
 
     console.log("Content Extraction completed...............");
 
-    if(results.length === 0) {
-
-      return res.status(201).json({ message: 'No Listing found on the pages..', data: results });
-
-    }else{
-
-      return res.status(201).json({ message: 'Listing result found.', data: results });
-      
+    if (results.length === 0) {
+      return res
+        .status(201)
+        .json({ message: "No Listing found on the pages..", data: results });
+    } else {
+      return res
+        .status(201)
+        .json({ message: "Listing result found.", data: results });
     }
-    
-
   } catch (error) {
     console.error("Error:", error);
     return res.status(500).json({ message: "Error while processing." });
   }
 });
-
-
-
 
 app.listen(port, "0.0.0.0", function () {
   console.log(`Listening on Port ${port}`);
